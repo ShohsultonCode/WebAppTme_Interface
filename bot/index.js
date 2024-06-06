4fa855fbdf986b4a87bbf1d4ca26deeecf09bcd3
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-06 19:17:02 +0500

    some

--- bot/index.js
+++ bot/index.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "6847595776:AAFGMoyl9Tm8ReuTHOqoP4trAp5axXDNC8o";
+const TOKEN = "7333536576:AAEy1MtDXijiY1NYEEMCFv3L73cpnDbWrek";
 const bot = new Telegraf(TOKEN);
 
 const web_link = "https://web-app-tme-interface.vercel.app/";
