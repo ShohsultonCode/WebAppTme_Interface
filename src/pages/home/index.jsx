6db1f0deb329eed8ca3572cbadb6343f0e0a1641
Shohsulton Ummatov <108561261+ShohsultonCode@users.noreply.github.com>
2024-06-07 02:05:02 +0500

    some

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';  
 
 const Index = () => {
     const [products, setProducts] = useState([]);
     const [userId, setUserId] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate(); 
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const userId = urlParams.get('user_id');
+        
         if (userId) {
             setUserId(userId);
         }
@@ -16,8 +20,10 @@ const Index = () => {
                 const response = await fetch('https://shohsulton.uz/webappbot/api/products');
                 const data = await response.json();
                 setProducts(data.data);
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching products:', error);
+                setLoading(false);
             }
         };
 
@@ -25,26 +31,37 @@ const Index = () => {
     }, []);
 
     return (
-        <div className="container">
-            <h1 className="mb-4">Products:</h1>
+        <div className="container mt-5">
+            <div className="d-flex justify-content-between align-items-center mb-4">
+                <h1 className="titlecha">Products:</h1>
+                    <button className="btn btn-success" onClick={() => navigate('/add/product')}>
+                        Add Product
+                    </button>
+            </div>
             {userId && <p>User ID: {userId}</p>}
-            <div className="row row-cols-2"> {/* Use row-cols-2 to display 2 columns */}
-                {products.map((product, index) => (
-                    <div className="col-6 mb-4 rounded" key={index}>
-                        <div className="card h-100">
-                            {product.product_image && (
-                                <img src={`https://shohsulton.uz/webappbot/api/images/${product.product_image}`} className="card-img-top img-fluid" alt={product.product_name} />
-                            )}
-                            <div className="card-body">
-                                <h5 className="card-title">{product.product_name}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Type: {product.product_category.category_name}</h6>
-                                <p className="card-text">Price: ${product.product_price.toFixed(2)}</p>
+            {loading ? (
+                <div className="d-flex justify-content-center">
+                    <div className="loader"></div>
+                </div>
+            ) : (
+                <div className="row row-cols-2">
+                    {products.map((product, index) => (
+                        <div className="col-6 mb-4 rounded" key={index}>
+                            <div className="card h-100">
+                                {product.product_image && (
+                                    <img src={`https://shohsulton.uz/webappbot/api/images/${product.product_image}`} className="card-img-top img-fluid" alt={product.product_name} />
+                                )}
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.product_name}</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Type: {product.product_category.category_name}</h6>
+                                    <p className="card-text">Price: ${product.product_price.toFixed(2)}</p>
                                     <button className="btn btn-primary buttoncha">Order</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
